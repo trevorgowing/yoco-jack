c423ca6a28230c4b029ef86457b77dded083ac83
= <trev.gowing@gmail.com>
2018-03-31 16:18:55 +0200

    Encapsulate calculator

---
+++ src/main/java/com/trevorgowing/Calculator.java
@@ -0,0 +1,55 @@
+package com.trevorgowing;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.OptionalInt;
+
+class Calculator {
+
+  boolean calculateWinner(List<Card> handA, List<Card> handB) {
+    Integer valueA = handA.stream()
+        .mapToInt(Card::getValue)
+        .sum();
+
+    Integer valueB = handB.stream()
+        .mapToInt(Card::getValue)
+        .sum();
+
+    if (valueA > 21) {
+      return false;
+    } else if (valueB > 21) {
+      return true;
+    } else if (Objects.equals(valueA, valueB)) {
+      return calculateHighestCardWinner(handA, handB);
+    } else {
+      return valueA > valueB;
+    }
+  }
+
+  private boolean calculateHighestCardWinner(List<Card> handA, List<Card> handB) {
+    OptionalInt valueA = handA.stream()
+        .mapToInt(Card::getValue)
+        .max();
+
+    OptionalInt valueB = handB.stream()
+        .mapToInt(Card::getValue)
+        .max();
+
+    if (!valueA.isPresent()) {
+      throw new IllegalArgumentException("Hand A has no cards");
+    }
+    if (!valueB.isPresent()) {
+      throw new IllegalArgumentException("Hand B has no cards");
+    }
+
+    Integer maxA = valueA.getAsInt();
+    Integer maxB = valueB.getAsInt();
+    int comparison = maxA.compareTo(maxB);
+
+    return comparison == 0 ? calculateSuiteWinner(handA, handB) : comparison > 0;
+  }
+
+  private boolean calculateSuiteWinner(List<Card> handA, List<Card> handB) {
+    return true;
+  }
+}
--- src/main/java/com/trevorgowing/Main.java
+++ src/main/java/com/trevorgowing/Main.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.OptionalInt;
 
 public class Main {
 
@@ -26,56 +24,9 @@ public class Main {
       System.out.println(handA);
       System.out.println(handA);
 
-      System.out.println("Calculated Winner: " + calculateWinner(handA, handB));
+      System.out.println("Calculated Winner: " + new Calculator().calculateWinner(handA, handB));
       System.out.println("Expected Winner: " + game.isPlayerAWins());
       System.out.println();
     }
-  }
-
-  private static boolean calculateWinner(List<Card> handA, List<Card> handB) {
-    Integer valueA = handA.stream()
-        .mapToInt(Card::getValue)
-        .sum();
-
-    Integer valueB = handB.stream()
-        .mapToInt(Card::getValue)
-        .sum();
-
-    if (valueA > 21) {
-      return false;
-    } else if (valueB > 21) {
-      return true;
-    } else if (Objects.equals(valueA, valueB)) {
-      return calculateHighestCardWinner(handA, handB);
-    } else {
-      return valueA > valueB;
-    }
-  }
-
-  private static boolean calculateHighestCardWinner(List<Card> handA, List<Card> handB) {
-    OptionalInt valueA = handA.stream()
-        .mapToInt(Card::getValue)
-        .max();
-
-    OptionalInt valueB = handB.stream()
-        .mapToInt(Card::getValue)
-        .max();
-
-    if (!valueA.isPresent()) {
-      throw new IllegalArgumentException("Hand A has no cards");
-    }
-    if (!valueB.isPresent()) {
-      throw new IllegalArgumentException("Hand B has no cards");
-    }
-
-    Integer maxA = valueA.getAsInt();
-    Integer maxB = valueB.getAsInt();
-    int comparison = maxA.compareTo(maxB);
-
-    return comparison == 0 ? calculateSuiteWinner(handA, handB) : comparison > 0;
-  }
-
-  private static boolean calculateSuiteWinner(List<Card> handA, List<Card> handB) {
-    return true;
   }
 }
